b088e5b3f15b34a53ca7d6c1cd38faa2817bd5b0
Earth <et.nk@hotmail.com>
2022-05-16 22:16:00 +0700

    link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
+
+import { MovieProps } from './interface/MovieProps';
 import logo from './logo.svg';
+import Form from './component/Form';
+import List from './component/List';
 import './App.css';
 
 function App() {
+  const [movieList, setMovieList] = React.useState<MovieProps["movieList"]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My Movies</h1>
+      <Form movieList={movieList} setMovieList={setMovieList} />
+      <List movieList={movieList} />
+    </>
   );
 }
 
